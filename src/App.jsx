db333b6a02897ddee5ee110f1f008b72c1c1d142
varshala28 <2020sp189@univ.jfn.ac.lk>
2025-02-23 09:15:19 +0530

    Updated hero.jsx and css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css'
+import './App.css';
+import Hero from './sections/Hero/Hero'
 
 function App() {
- 
   return (
     <>
-    App
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,64 @@
+import heroImg from '../../assets/hero-img.png'
+import styles from './HeroStyles.module.css'
+import themeIcon from '../../assets/sun.svg'
+import githubIcon from '../../assets/github-light.svg'
+import LinkedinIcon from '../../assets/linkedin-light.svg'
+import Cv from '../../assets/cv.pdf'
+import { useTheme } from '../../common/ThemeContext'
+import sun from '../../assets/sun.svg'
+import moon from '../../assets/moon.svg'
+import githubLight from '../../assets/github-light.svg'
+import githubDark from '../../assets/github-dark.svg'
+import LinkedinLight from '../../assets/linkedin-light.svg'
+import LinkedinDark from '../../assets/linkedin-dark.svg'
+
+function Hero () {
+  const {theme,toggleTheme} = useTheme();
+
+  const themeIcon = theme ==='light' ? sun : moon;
+  const githubIcon = theme ==='light' ? githubLight : githubDark;
+  const LinkedinIcon = theme ==='light' ? LinkedinLight : LinkedinDark;
+  
+  return ( 
+    <section id="hero" className={styles.container}> 
+     <div className={styles.colorModeContainer}>
+        <img 
+          className={styles.hero}
+          src={heroImg}
+          alt="Profile picture of Miss Varsha"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Varsha 
+          <br />
+          Thayananthan 
+        </h1>
+        <h2> Frontend Developer </h2>
+        <span>
+          <a href="https://github.com/" target="_blank">
+          <img src={githubIcon} alt="Github icon" />
+          </a>
+          <a href="https://Linkedin.com/" target="_blank">
+          <img src={LinkedinIcon} alt="Linkedin icon" />
+          </a>
+        </span>
+        <p>
+          With a passion for developing modern React web apps for 
+          commercial business.
+        </p>
+        <a href={Cv} download>
+          <button className="hover" > Resume</button>
+        </a>
+      </div>
+      </section>
+    )
+  }
+
+export default Hero
